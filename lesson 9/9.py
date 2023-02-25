a592dcc1378f5c3ae7617443e1025eb3a5fe901f
Dimabrishnikov <tendramore@gmail.com>
2023-02-25 12:27:59 +0200

    dz 9

--- lesson 9/9.py
+++ lesson 9/9.py
@@ -29,3 +29,16 @@ if response.status_code == 200:
     soup_text = soup.find_all("a", {"href" : "/currencies/bitcoin/markets/"})
     res = soup_text[0].find("span")
     print(res.text)
+
+dic_curr = {}
+
+response = requests.get("https://bank.gov.ua/ua/markets/exchangerates")
+if response.status_code == 200:
+    soup = BeautifulSoup(response.text, features="html.parser")
+    cur_kod = soup.find_all("td", {"data-label": "Код літерний"})
+    cur_val = soup.find_all("td", {"data-label": "Офіційний курс"})
+    for i in range(len(cur_kod)):
+        dic_curr[cur_kod[i].text] = cur_val[i].text
+
+print(dic_curr)
+print(dic_curr['USD'])
